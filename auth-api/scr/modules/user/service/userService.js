90016f91a842a5ba2cfe41e535a3d20072a67dbf
all-an <all-an@hotmail.com>
2021-12-22 08:50:38 -0300

    learning

--- auth-api/scr/modules/user/service/userService.js
+++ auth-api/scr/modules/user/service/userService.js
@@ -1,7 +1,21 @@
 import UserRepository from "../repository/UserRepository";
+import * as httpStatus from "../../../config/constants/httpStatus.js"
 
 class UserService{
+    async findByEmail(req){
+        try {
+            const { email } = req.params;
+            this.validarDadosRequisicao(email);
+        } catch (err) {
+            
+        }
+    }
 
+    validarDadosRequisicao(email){
+        if(!email){
+            throw new Error("User email was not informed");
+        }
+    }
 }
 
 export default new UserService();
